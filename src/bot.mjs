0169cf201f5f69d7bebe6bf03cd62651e16fafda
Burak <benburakyaa@gmail.com>
2024-04-21 19:11:12 +0300

    test

--- src/bot.mjs
+++ src/bot.mjs
@@ -11,6 +11,7 @@
  * 		21.04.2024, 19:03:09
  */
 
+import 'dotenv/config'
 import { Client, GatewayIntentBits } from "discord.js";
 
 const client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
@@ -19,9 +20,4 @@ client.once('ready', () => {
     console.log(`Bot Başlatıldı: ${client.user.tag}`);
 });
 
-const e = 'MTIyNTU2MDMwOTcyOTU5NTQ4Mg';
-const x = 'Gy2mS-';
-const p = 'PNe14EWMWtUDOwmd89AEIo5ckM6Q9h';
-const t = 'OaJpOZU';
-
-client.login(`${e}.${x}.${p}-${t}`);
+client.login(process.env.TOKEN);
